efa84063fb09ca2a741ab55f97043b624650ce68
Antonio Delgado <tonydelro@gmail.com>
2025-11-13 16:30:46 -0600

    CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import cors from '@fastify/cors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -14,6 +15,7 @@ async function bootstrap() {
   app.enableCors({
     origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    credentials: true,
   });
 
   await app.listen(3000, '0.0.0.0');
