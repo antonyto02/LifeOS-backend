c0a306a5a19ee361b39d2dc31c4fb2ffe95d3350
Antonio Delgado <tonydelro@gmail.com>
2025-11-21 17:12:54 -0600

    Fix collision snapshot type error

--- src/investments/bot/decisions/computeCollisionPoint.ts
+++ src/investments/bot/decisions/computeCollisionPoint.ts
@@ -64,7 +64,7 @@ export function computeCollision(symbol: string): CollisionSnapshot | undefined 
   const topAsk = askDepth / depthSum;
 
   return {
-    bidPrice: parseFloat(bidPrice),
+    bidPrice,
     secondBidPrice,
     askPrice: parseFloat(askPrice),
     topBid,
