1e88c489fd471ba8a5121445afb1605af5fce1c6
Antonio Delgado <tonydelro@gmail.com>
2025-11-21 09:52:42 -0600

    Add periodic Binance user stream keepalive

--- src/investments/stream/binance-user-stream.service.ts
+++ src/investments/stream/binance-user-stream.service.ts
@@ -1,11 +1,12 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import axios from 'axios';
 import WebSocket from 'ws';
 import { UserEventsLogic } from '../logic/user-events.logic';
 
 @Injectable()
-export class BinanceUserStreamService implements OnModuleInit {
+export class BinanceUserStreamService implements OnModuleInit, OnModuleDestroy {
   private ws: WebSocket;
+  private keepAliveInterval?: NodeJS.Timeout;
 
   constructor(
     private readonly logicService: UserEventsLogic,
@@ -13,7 +14,14 @@ export class BinanceUserStreamService implements OnModuleInit {
 
   async onModuleInit() {
     const listenKey = await this.createListenKey();
+    this.startKeepAlive(listenKey);
     this.connect(listenKey);
+  }
+
+  onModuleDestroy() {
+    if (this.keepAliveInterval) {
+      clearInterval(this.keepAliveInterval);
+    }
   }
 
   private async createListenKey(): Promise<string> {
@@ -28,6 +36,27 @@ export class BinanceUserStreamService implements OnModuleInit {
     );
 
     return response.data.listenKey;
+  }
+
+  private startKeepAlive(listenKey: string) {
+    const renew = async () => {
+      try {
+        await axios.put(
+          'https://api.binance.com/api/v3/userDataStream',
+          null,
+          {
+            params: { listenKey },
+            headers: {
+              'X-MBX-APIKEY': process.env.BINANCE_API_KEY,
+            },
+          },
+        );
+      } catch (error) {
+        // Ignore errors to avoid disrupting the stream; next interval will retry
+      }
+    };
+
+    this.keepAliveInterval = setInterval(renew, 30 * 60 * 1000);
   }
 
   private connect(listenKey: string) {
